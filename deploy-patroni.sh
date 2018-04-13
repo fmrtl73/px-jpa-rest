92222bfc9c1297adfbeac64922a0c7eda248b082
root <root@px-k8s-master.c.trek-treckr-184614.internal>
2018-04-13 14:51:34 +0000

    fix

--- deploy-patroni.sh
+++ deploy-patroni.sh
@@ -1,4 +1,3 @@
-ETCD_HOST_URL=10.128.0.2:2379
 kubectl create -f k8s-yaml/px-repl1-sc.yaml
 helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
-helm install --name px --set persistentVolume.storageClass=px-repl1-sc,Etcd.Host=$ETCD_HOST_URL,Etcd.DeployChart=false,Replicas=3 incubator/patroni
+helm install --name px -f k8s-yaml/patroni-values.yaml incubator/patroni
